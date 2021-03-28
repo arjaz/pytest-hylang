fde2f326c4009844a9fe4bf1e7023d0b5b8200e5
Arjaz <arjaz@protonmail.com>
2021-03-28 13:57:48 +0300

    feat: add plumbery for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pytest-hylang",
     version="0.0.1",
-    description="Pytest plugin to allow running tests written in hylang",
-    packages=["pytest_hylang"],
     author="Eugene Rossokha",
+    author_email="arjaz@protonmail.com",
+    description="Pytest plugin to allow running tests written in hylang",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arjaz/pytest-hylang",
+    packages=["pytest_hylang"],
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["hy.pytest_plugin = pytest_hylang.conftest"]},
     # custom PyPI classifier for pytest plugins
-    classifiers=["Framework :: Pytest"],
+    classifiers=[
+        "Framework :: Pytest",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=["pytest", "py", "hy"],
+    python_requires=">=3.6",
 )
